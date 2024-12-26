b4eeef997f301c8a55571a8703ced828354ce8bc
mrlk <test-9w3r8ll8w@srv1.mail-tester.com>
2024-12-26 10:18:04 +0000

    fix directory

---
+++ src/main/java/com/mar/service/Questions.java
@@ -0,0 +1,21 @@
+package com.mar.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Questions implements Serializable {
+
+    private List<Question> questions;
+
+}
